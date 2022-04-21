358909cf0d62256fb8828342bf19e4cb5d79920c
Ljupco20 <ljupcotrajanovski@hotmail.com>
2022-04-21 21:00:01 +0200

    Working on Fitness site with Bootstrap 4.6

---
+++ 12_Bootstrap/Fitness-Bootsrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Ljupco Trajanovski
+    File Name: script.js 
+    Date: 21.04.2022
+*/
+
+// jquery for hero image to consume the header window space
+$(document).ready(function () {
+    $(".hero").height($(window).height())
+});
